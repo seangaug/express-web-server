296d98877af15a2fb4cb95dc1b452f90478875a9
seangaug <seanjnr1991@aol.com>
2023-02-20 11:47:33 +0000

    express server connected

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+// Serve the static content of the public directory using express.static middleware
+app.use(express.static('public'));
+
+// Make sure you connect your server file to port 3000
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
